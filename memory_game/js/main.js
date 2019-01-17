9ad2b9f7ffee2f318e49900fa20dd57743a175a6
Sam Kahn <samkahn101@gmail.com>
2019-01-16 20:04:22 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,20 @@
-console.log("Up and running!");
-var cardOne = 'queen';
-var cardTwo = 'king';
-var cardThree = 'queen';
-var vardFour = 'king';
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+var cardOne;
+var cardTwo;
+cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log('user flipped ' + cardsInPlay[0]);
+cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log('user flipped ' + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert('You found a match!');
+	} else {
+		alert("Sorry, try again.");
+	}
+} else {
+	console.log('pick another card');
+}
