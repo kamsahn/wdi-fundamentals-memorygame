f2aa373a3e5c28fd569931e866c32a5f8aa1e63a
Sam Kahn <samkahn101@gmail.com>
2019-01-18 22:37:16 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -21,7 +21,9 @@ var cards = [
 }
 ];
 var cardsInPlay = [];
-var checkForMatch = function() {
+var checkForMatch = function(elem) {
+	var index = elem.getAttribute('data-id');
+	elem.setAttribute('src', cards[index].cardImage);
 	if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert('You found a match!');
@@ -32,17 +34,26 @@ var checkForMatch = function() {
 	console.log('pick another card');
 	}
 };
-var flipCard = function(cardId) {
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
 	console.log('A card has been flipped.')
 	cardsInPlay.push(cards[cardId].rank);
 	console.log('user flipped ' + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-	checkForMatch();
+	checkForMatch(this);
+};
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	};
 };
 
-flipCard(0);
-flipCard(2);
+createBoard();
 
 
 
